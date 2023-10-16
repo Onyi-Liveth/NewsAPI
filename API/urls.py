0eac595ee46c92278436fe3468b9edcf9c316052
Onyinye Liveth <onyiorajiaku1@gmail.com>
2023-10-16 08:12:48 +0100

    url

--- API/urls.py
+++ API/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import WebNews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', WebNews.as_view(), name='news'),
+
 ]
